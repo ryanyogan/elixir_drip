d222d9fdd5137fd3f1a3012d0f8bb3ed52d5bc8b
ryanyogan <ryanyogan@gmail.com>
2019-10-13 13:28:17 -0700

    added in mix alias for linting

--- mix.exs
+++ mix.exs
@@ -5,13 +5,20 @@ defmodule ElixirDrip.Umbrella.MixProject do
     [
       apps_path: "apps",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
   defp deps do
     [
       {:credo, "~> 0.3", only: [:test, :dev], runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      lint: ["format", "credo"]
     ]
   end
 end
