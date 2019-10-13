99c244ab7066f845ecaf90034a69626cae2651a5
ryanyogan <ryanyogan@gmail.com>
2019-10-13 11:25:34 -0700

    Added in the presentation layer

---
+++ apps/elixir_drip_web/config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :elixir_drip_web,
+  namespace: ElixirDripWeb,
+  ecto_repos: [ElixirDripWeb.Repo]
+
+# Configures the endpoint
+config :elixir_drip_web, ElixirDripWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "jI/tbdIcm6IjMD1SVJRiO7WyYoFQGx5Okc/veDleQ63iqZX0ndqxkNvrm0Yw7gbo",
+  render_errors: [view: ElixirDripWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: ElixirDripWeb.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+config :elixir_drip_web, :generators,
+  context_app: false
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
